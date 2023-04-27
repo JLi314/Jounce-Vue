67056de36c891804bab62055be9af15e2f153a52
jiahua.li12321@gmail.com <Jiahua.li12321@gmail.com>
2023-04-26 20:11:48 -0400

    Initialized Vue, Vite, and Axios

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const test = () => {
+  console.log("reached here")
+}
+</script>
+
+<template>
+  <select @change="test">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+</template>
+
+<style scoped>
+</style>
