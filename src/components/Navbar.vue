2c9e4c146aceb38d01559ef0217670ee912811ae
jiahua.li12321@gmail.com <Jiahua.li12321@gmail.com>
2023-05-24 22:27:15 -0400

    Finished transitionings views to components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<script setup></script>
+
+<template>
+  <div class="topnav">
+    <a class="active" href="">Home</a>
+    <a href="">Store</a>
+    <a href="">Login</a>
+  </div>
+</template>
+
+<style scoped>
+.topnav {
+  overflow: hidden;
+  background-color: #000000;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: rgb(255, 255, 255);
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  font-family: "Montserrat", sans-serif;
+}
+
+.topnav a:hover {
+  background-color: whitesmoke;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: rgb(80, 80, 80);
+  color: white;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav a,
+  .topnav input[type="text"] {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+}
+</style>
