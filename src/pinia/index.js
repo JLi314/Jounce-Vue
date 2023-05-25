ebad22fcc6d48febf75af42d8c99d2c910624f28
jiahua.li12321@gmail.com <Jiahua.li12321@gmail.com>
2023-05-24 22:30:19 -0400

    Prettiered it

--- src/pinia/index.js
+++ src/pinia/index.js
@@ -1,14 +1,14 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 
-const useStore = defineStore('store', {
+const useStore = defineStore("store", {
   state: () => ({
     user: "Big Boy Billy",
   }),
   actions: {
     greeting() {
       console.log(`Hello ${this.user}!`);
-    }
-  }
+    },
+  },
 });
 
 export default useStore;
