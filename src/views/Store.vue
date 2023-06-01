cb1940b64031804d71cd82c3380d85c800154d17
jiahua.li12321@gmail.com <Jiahua.li12321@gmail.com>
2023-05-31 21:12:21 -0400

    Added modal functionality

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,28 +1,38 @@
 <script setup>
 import axios from "axios";
 import { ref } from "vue";
+import Navbar from "../components/Navbar.vue";
+import Modal from "../components/Modal.vue";
 
+const showModal = ref(false);
+const selectedRecordId = ref(0);
 const movies = ref(null);
 
+const toggleModal = (id) => {
+  showModal.value = !showModal.value;
+  selectedRecordId.value = id;
+};
+
 movies.value = (
-  await axios.get("https://api.themoviedb.org/3/movie/popular", {
-    params: {
-      api_key: import.meta.env.VITE_API_KEY,
-      region: "US",
-      language: "en",
-      include_adult: false,
-    },
-  })
+  await axios.get(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${
+      import.meta.env.VITE_API_KEY
+    }&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_watch_monetization_types=flatrate&with_genres=99&with_keywords=9882`
+  )
 ).data;
-console.log(movies.value);
 </script>
 
 <template>
+  <Navbar />
   <div v-if="movies" class="tiles">
     <div v-for="movie in movies.results">
-      <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" />
+      <img
+        :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+        @click="toggleModal(movie.id)"
+      />
     </div>
   </div>
+  <Modal v-if="showModal" :id="selectedRecordId" @toggleModal="toggleModal()" />
 </template>
 
 <style scoped>
@@ -33,5 +43,6 @@ console.log(movies.value);
 }
 img {
   width: 250px;
+  height: 375px;
 }
 </style>
