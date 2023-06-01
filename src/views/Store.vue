337fee16506b9c5e9de6c0fa893e110248c3eacf
jiahua.li12321@gmail.com <Jiahua.li12321@gmail.com>
2023-05-31 20:01:02 -0400

    Added movie posters

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,12 +1,37 @@
-<script>
-import { ref } from "vue";
+<script setup>
 import axios from "axios";
+import { ref } from "vue";
 
-axios.get(`https://api.themoviedb.org/3/discover/movie`, {
-  params: {},
-});
+const movies = ref(null);
+
+movies.value = (
+  await axios.get("https://api.themoviedb.org/3/movie/popular", {
+    params: {
+      api_key: import.meta.env.VITE_API_KEY,
+      region: "US",
+      language: "en",
+      include_adult: false,
+    },
+  })
+).data;
+console.log(movies.value);
 </script>
 
-<template></template>
+<template>
+  <div v-if="movies" class="tiles">
+    <div v-for="movie in movies.results">
+      <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" />
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.tiles {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  align-content: center;
+}
+img {
+  width: 250px;
+}
+</style>
