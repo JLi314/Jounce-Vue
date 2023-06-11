1b46f6d5d83c9e71ea35b6e62b99f9c0d8532e4e
jiahua.li12321@gmail.com <Jiahua.li12321@gmail.com>
2023-06-11 02:06:30 -0400

    Added genre dropdown and search feature

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,13 +1,15 @@
 <script setup>
 import axios from "axios";
 import { ref } from "vue";
-import { Transition } from "vue";
 import Navbar from "../components/Navbar.vue";
 import Modal from "../components/Modal.vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
 const showModal = ref(false);
+const genre = ref(99);
+const search = ref("");
+const totalPages = ref(0);
 const selectedRecordId = ref(0);
 const movies = ref(null);
 
@@ -16,17 +18,85 @@ const toggleModal = (id) => {
   selectedRecordId.value = id;
 };
 
-movies.value = (
-  await axios.get(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${
-      import.meta.env.VITE_API_KEY
-    }&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_watch_monetization_types=flatrate&with_genres=99&with_keywords=9882`
-  )
-).data;
+const getTMDBData = async (url, options, page) => {
+  movies.value = (
+    await axios.get(url, {
+      params: {
+        api_key: import.meta.env.VITE_API_KEY,
+        region: "US",
+        language: "en",
+        include_adult: false,
+        page,
+        ...options,
+      },
+    })
+  ).data;
+  totalPages.value = movies.value.total_pages;
+  console.log(movies.value);
+};
+
+console.log;
 </script>
 
 <template>
   <Navbar />
+  <div>
+    <input type="search" placeholder="Barbie is behind you" v-model="search" />
+    <button
+      @click="
+        getTMDBData('https://api.themoviedb.org/3/search/movie', {
+          query: search,
+        })
+      "
+    >
+      Search
+    </button>
+    <select v-model="genre">
+      <option value="99">Documentary</option>
+      <option value="35">Comedy</option>
+      <option value="878">Science Fiction</option>
+    </select>
+    <button
+      @click="
+        getTMDBData('https://api.themoviedb.org/3/discover/movie', {
+          with_genres: genre,
+        })
+      "
+    >
+      Fetch
+    </button>
+  </div>
+  <div>
+    <button
+      class="nav"
+      @click="
+        getTMDBData(
+          'https://api.themoviedb.org/3/search/movie',
+          {
+            query: search,
+          },
+          page--
+        )
+      "
+    >
+      Previous
+    </button>
+    <p>{{ `Page ${page} of ${totalPages}` }}</p>
+    <button
+      class="nav"
+      @click="
+        getTMDBData(
+          'https://api.themoviedb.org/3/search/movie',
+          {
+            query: search,
+          },
+          page++
+        )
+      "
+    >
+      Next
+    </button>
+  </div>
   <div v-if="movies" class="tiles">
     <div v-for="movie in movies.results">
       <img
@@ -48,5 +118,9 @@ movies.value = (
 img {
   width: 250px;
   height: 375px;
+}
+
+.nav {
+  color: white;
 }
 </style>
